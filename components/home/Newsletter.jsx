a92ab289ac2da990b201d2e3e74b1f61e09c0658
kundan <kundan8789@gmail.com>
2026-05-06 23:26:31 +0530

    some change

--- components/home/Newsletter.jsx
+++ components/home/Newsletter.jsx
@@ -29,37 +29,7 @@ export default function Newsletter() {
           Get exclusive deals, new arrivals & special offers
         </motion.p>
 
-        {/* Input + Button */}
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.4 }}
-          className="mt-10 flex flex-col md:flex-row items-center justify-center gap-4"
-        >
-
-          <input
-            type="email"
-            placeholder="Enter your email"
-            className="w-full md:w-[380px] px-5 py-3 rounded-full 
-            bg-gray-100 border border-gray-300 
-            focus:outline-none focus:border-black 
-            transition"
-          />
-
-          <button className="px-8 py-3 rounded-full font-medium 
-          bg-black text-white 
-          hover:bg-gray-800 
-          transition-all duration-300 hover:scale-105">
-            Subscribe
-          </button>
-
-        </motion.div>
-
-        {/* Small note */}
-        <p className="text-xs text-gray-500 mt-4">
-          No spam. Only good stuff.
-        </p>
-
+       
       </div>
 
     </section>
